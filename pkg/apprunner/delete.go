549b8360cda0ff9ea7bdf7938d47f4e40b69ab30
ohkinozomu <nozomunoise@gmail.com>
2022-02-01 18:37:37 +0900

    Add polling in DeleteService

--- pkg/apprunner/delete.go
+++ pkg/apprunner/delete.go
@@ -3,9 +3,28 @@ package apprunner
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/apprunner"
+	"github.com/briandowns/spinner"
 )
+
+func (ar *AppRunner) waitForDeleted() {
+	s := spinner.New(spinner.CharSets[43], 100*time.Millisecond)
+	s.Start()
+	// TODO: set timeout
+	for {
+		exists, err := ar.ServiceExists()
+		if err != nil {
+			panic(err)
+		}
+		if !exists {
+			break
+		}
+		time.Sleep(time.Second * 5)
+	}
+	s.Stop()
+}
 
 // https://docs.aws.amazon.com/apprunner/latest/api/API_DeleteService.html
 func (ar *AppRunner) DeleteService() error {
@@ -20,6 +39,7 @@ func (ar *AppRunner) DeleteService() error {
 	if err != nil {
 		return err
 	}
+	ar.waitForDeleted()
 	fmt.Println("Deleted.")
 	return nil
 }
