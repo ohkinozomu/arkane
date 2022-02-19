c34dd112d9150382a279121e4fdaeca644796ef3
ohkinozomu <nozomunoise@gmail.com>
2022-02-19 16:29:25 +0900

    Add --dry-run flag

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,53 +1,71 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
+	"errors"
+	"log"
 
 	"github.com/ohkinozomu/arkane/pkg/apprunner"
 	"github.com/ohkinozomu/arkane/pkg/knative"
 	"github.com/spf13/cobra"
 )
 
+var (
+	dryRun bool
+)
+
 func init() {
 	rootCmd.AddCommand(applyCmd)
-	applyCmd.PersistentFlags().StringVarP(&fileName, "file", "f", "", "file name")
+	applyCmd.Flags().StringVarP(&fileName, "file", "f", "", "file name")
+	applyCmd.Flags().BoolVar(&dryRun, "dry-run", false, "dry run")
 }
 
 var applyCmd = &cobra.Command{
 	Use:   "apply",
 	Short: "apply service",
 	Long:  `apply service`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if dryRun {
+			log.Println("dry run")
+		}
+
 		if fileName == "" {
-			fmt.Println("Error: Input --file")
-			os.Exit(1)
+			return errors.New("input --file")
 		}
 
 		svc, err := knative.Parse(fileName)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
-		ar, err := apprunner.New(svc)
+		err = knative.Validate(svc)
 		if err != nil {
-			panic(err)
+			return err
+		}
+		log.Println("validation OK")
+
+		if !dryRun {
+			ar, err := apprunner.New(svc)
+			if err != nil {
+				return err
+			}
+
+			serviceExists, err := ar.ServiceExists()
+			if err != nil {
+				return err
+			}
+			if serviceExists {
+				err = ar.UpdateService()
+				if err != nil {
+					return err
+				}
+			} else {
+				err = ar.CreateService()
+				if err != nil {
+					return err
+				}
+			}
 		}
 
-		serviceExists, err := ar.ServiceExists()
-		if err != nil {
-			panic(err)
-		}
-		if serviceExists {
-			err = ar.UpdateService()
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			err = ar.CreateService()
-			if err != nil {
-				panic(err)
-			}
-		}
+		return nil
 	},
 }
