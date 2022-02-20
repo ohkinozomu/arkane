2f60b92d05ce9b48d3b668f9f5b4bd84c17b6a59
Nozomu Ohki <38576286+ohkinozomu@users.noreply.github.com>
2022-02-20 23:51:09 +0900

    Validate multiple-containers (#24)

--- pkg/knative/validate_test.go
+++ pkg/knative/validate_test.go
@@ -1,17 +1,36 @@
 package knative
 
 import (
+	"errors"
 	"testing"
 )
 
+type test struct {
+	file        string
+	validateErr error
+}
+
 func TestValidate(t *testing.T) {
-	svc, err := Parse("../../test/sample.yaml")
-	if err != nil {
-		t.Fatalf("failed test: %v", err)
+	tests := []test{
+		test{
+			file:        "../../test/sample.yaml",
+			validateErr: nil,
+		},
+		test{
+			file:        "../../test/multiple_containers.yaml",
+			validateErr: errors.New("[ERROR] App Runner doesn't support multiple containers: https://github.com/aws/apprunner-roadmap/issues/71"),
+		},
 	}
 
-	err = Validate(svc)
-	if err != nil {
-		t.Fatalf("failed test: %v", err)
+	for _, test := range tests {
+		svc, err := Parse(test.file)
+		if err != nil {
+			t.Fatalf("failed test: %v", err)
+		}
+
+		err = Validate(svc)
+		if err != nil && err.Error() != test.validateErr.Error() {
+			t.Fatalf("failed test: %v", err)
+		}
 	}
 }
